e0fcec643ae2d789b35e35ae8f8e6bed9f55c2b9
doniaabdelmoneim <donia.abdelmoneim19@gmail.com>
2025-07-07 01:52:52 +0300

    add files

---
+++ E-commerce/src/app/components/nav-auth/nav-auth.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-auth',
+  imports: [],
+  templateUrl: './nav-auth.html',
+  styleUrl: './nav-auth.scss'
+})
+export class NavAuth {
+
+}
